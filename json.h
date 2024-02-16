a3de4a3bb9b095494c96ebe881863fa4c72bc2f8
rain8128 <126866571+rain8128@users.noreply.github.com>
2024-02-16 22:57:52 +0900

    Change defs

--- json.h
+++ json.h
@@ -1,9 +1,25 @@
+#include <stdint.h>
+
 enum {
     JSON_NULL = 0,
-    JSON_TRUE,
-    JSON_FALSE,
+    JSON_BOOL,
     JSON_STRING,
-    JSON_NUMBER,
+    JSON_INTEGER,
+    JSON_REAL,
     JSON_ARRAY,
     JSON_OBJECT
+};
+
+typedef struct json_array json_array;
+typedef struct json_object json_object;
+
+typedef struct {
+    int type;
+    union value{
+        int64_t num_int;
+        double num_real;
+        char* string;
+        json_array* array;
+        json_object* object;
+    };
 };
